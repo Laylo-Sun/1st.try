06babb0e50c68e88546c68fb7c8dd8f2e8a6bcbc
Laylo-Sun <laylo.thesun@gmail.com>
2023-02-05 14:08:46 +0900

    Number, String, Type conversions

---
+++ class1.js
@@ -0,0 +1,13 @@
+
+
+// for( i=1;i<12;i++){
+//     console.log(i%2===0?`${i} juft son`:`${i} toq son `);
+// }
+
+
+// for(let i=1; i<=10; i++){
+//     for(let j=1; j<=10; j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     console.log(`=======================`);
+// }
